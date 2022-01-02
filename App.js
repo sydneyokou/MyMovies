8a1f78d5c4b9d4f134198f6ef4f5f6aafaae40ff
Maxime okou <maximeokou@air-de-maxime.home>
2022-01-02 18:53:56 +0100

    Persistence données

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+// App.js
+
+import React from 'react'
 import Navigation from './Navigation/Navigation'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux';
 import Store from './Store/configureStore'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
-export default function App() {
-  return (
-    <Provider store={Store}>
-      <Navigation/>
-    </Provider>
-    
-  );
+export default class App extends React.Component {
+  render() {
+    let persistor = persistStore(Store)
+    return (
+      <Provider store={Store}>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
+      </Provider>
+    )
+  }
 }
-
 
--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,7 +1,14 @@
 // Store/configureStore.js
 
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux'
 import toggleFavorite from './Reducers/favoriteReducer'
 import setAvatar from './Reducers/avatarReducer'
+import { persistCombineReducers } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default createStore(combineReducers({toggleFavorite, setAvatar}))
+const rootPersistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}))
